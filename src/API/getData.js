2115d1f396ba0121f186aae11247821fcf996120
kinzzzer <88973749+kinzzzer@users.noreply.github.com>
2022-07-07 18:49:29 +0300

    Delete getData.js

--- src/API/getData.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-
-export default async function getData() {
-    return  await axios
-        .get(`https://lldev.thespacedevs.com/2.2.0/launch?=limit=10`)
-        .then((res) => res.data.results)
-}
