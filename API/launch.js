95eec4525b9047544d3cbfd11543f71586b0a180
kinzzzer <88973749+kinzzzer@users.noreply.github.com>
2022-07-07 18:47:20 +0300

    Delete API directory

--- API/launch.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-
-export default async function getLaunchList(pagination) {
-    const query = queryString.stringify(pagination)
-    return  await axios
-        .get(`https://lldev.thespacedevs.com/2.2.0/launch?${query}`)
-        .then((res) => res.data.results)
-}
